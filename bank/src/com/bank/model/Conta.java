5691e129ae27d9120a445c94f5736453a166865a
Duda Sena <duda.sruiz@gmail.com>
2026-01-14 14:18:32 -0300

    feat: implement deposit and withdraw methods with business rules

--- bank/src/com/bank/model/Conta.java
+++ bank/src/com/bank/model/Conta.java
@@ -22,4 +22,23 @@ public class Conta {
 
         System.out.println("Conta criada! Número: " + this.numero);
     }
-}
+
+    //métodos
+    public void depositar(double valor) {
+        if (valor > 0) {
+            this.saldo += valor;
+        } else {
+            System.out.println("O valor é negativo, não é possível depositar.");
+        }
+    }
+
+        public void sacar(double valor) {
+            if (saldo > valor) {
+                this.saldo -= valor;
+                System.out.println("Valor retirado. O saldo restante é " + saldo);
+            } else {
+                System.out.println("Saldo insuficiente. O máximo disponível é " + saldo);
+            }
+    }
+    }
+
